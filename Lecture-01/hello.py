43ec0241a7cad9483e0e2c94737ad629fa072b5c
PEEMAK2004 <supakorn2547.peem@gmail.com>
2026-06-30 15:32:11 +0700

    intro 1st week

---
+++ Lecture-01/hello.py
@@ -0,0 +1,8 @@
+print("hello world")
+print("Information and Network Engineering")
+print("Supakorn You will be great")
+
+print("I'm Hungry")
+print('He ask me "who got the book"')  
+
+print("I am learning Python")
---
+++ Lecture-01/print.py
@@ -0,0 +1,2 @@
+print("I'm Supakorn", "I'll keep racticing!")
+print("I'm Supakorn " + "I'll keep racticing!")
